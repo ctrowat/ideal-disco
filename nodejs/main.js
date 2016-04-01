4f5c5f83abfc044987e7e57ee56abf6f66cacd94
Chris Rowat <chrisrowat@Chriss-iMac.local>
2016-04-01 06:09:09 -0600

    Adding streaming
    Some rainbow streaming code to test UDP data blasting.  The end result?
     Not very good, smooth for a while then it breaks up badly

--- nodejs/main.js
+++ nodejs/main.js
@@ -7,30 +7,73 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var PORT = 3000;
-var ESPHOST = '172.16.29.34';
+var ESPHOST = '192.168.4.1';
 var ESPUDPPORT = 7777;
 var ESPTCPPORT = 80;
+var ledCount = 30;
+var cnt = 0;
+var packets = 0;
+var sending = false;
+
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
 
 app.get('/testing', function(req, res) {
   res.send('Hello World!');
 });
-app.get('/sendudp', function(req, res) {
-  //var message = new Buffer(req.body.message);
+var updateLEDs = function() {
   var arr = [0x00, 0x00, 0x00];
-  for (var i = 0;i < 512;i++) {
-    arr = arr.concat([0xff,0xff,0xff]);
+  for (var i = 0;i < ledCount;i++) {
+    var colour = HSVtoRGB(((cnt + i) / ledCount) % 1, 1, 1);
+    arr = arr.concat([0xff & colour.g, 0xff & colour.r, 0xff & colour.b]);    
   }
+  cnt++;
+  if (cnt > ledCount) { cnt = 0; }
   var buff = new Buffer(arr);
   var client = dgram.createSocket('udp4');
   client.send(buff, 0, buff.length, ESPUDPPORT, ESPHOST, function(err) {
     if (err) {
       console.log('error sending datagram:');
       console.log(err);
+    } else {
+      console.log('sent ' + arr.length + ' bytes - packet ' + ++packets);
+      setTimeout(updateLEDs, 20);
     }
     client.close();
   });
+};
+var timeoutFunc;
+app.get('/sendudp', function(req, res) {
+    if (sending) {        
+      sending = false;
+      clearTimeout(timeoutFunc);
+    } else {
+      timeoutFunc = setTimeout(updateLEDs, 20);
+      sending = true;
+    }
   res.send('Worked');
-  console.log('sent');
 });
 app.post('/sendpost', function(req, res) {
   // maybe try grabbing the data from the post request and wrapping it into the one that goes to the ESP
